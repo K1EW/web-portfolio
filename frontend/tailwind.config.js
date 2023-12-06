12bacefb3c3280242bd626e927fdf23249e31afb
WeerawatW <weerawatwmt@gmail.com>
2023-12-06 23:32:33 +0700

    fix navbar for super small screen size

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "ssm": "420px", 
+      },
+    },
   },
   plugins: [],
 }
